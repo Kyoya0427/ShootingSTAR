82518fba4b41da30aa1124da2822cb7d90ac7a7f
kyoya0427 <rcbb.kyoya27@gmail.com>
2019-04-15 17:28:56 +0900

    no message

--- Assets/Script/Operation/PhysicalBehaviorStop.cs
+++ Assets/Script/Operation/PhysicalBehaviorStop.cs
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -93,3 +94,51 @@ public class PhysicalBehaviorStop : MonoBehaviour
     }
 }
 >>>>>>> 35a89303ea383694624dc56d0e990b444a2fd213
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class PhysicalBehaviorStop : MonoBehaviour
+{
+   
+    private Vector3 startPosition;
+    public GameObject _circle;
+    public Rigidbody2D _circleRigid;
+    public GameObject _btn;
+
+    Image _btnImage;
+    public Sprite _stopSprite;
+    public Sprite _playSprite;
+
+    void Start()
+    {
+        startPosition = _circle.transform.position;
+        _btnImage = _btn.GetComponent<Image>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool btnSwitchFlag = _btn.GetComponent<PlayAndStop>()._clickFlag;
+
+        //stateの変化後の処理
+        if (btnSwitchFlag == true)
+        {
+            _btnImage.sprite = _stopSprite;
+            Time.timeScale = 1.0f;
+        }
+        if (btnSwitchFlag == false)
+        {
+            _btnImage.sprite = _playSprite;
+            _circle.transform.position = startPosition;
+            _circleRigid.velocity = Vector3.zero;
+            Time.timeScale = 0.0f;
+        }
+    }
+}
+>>>>>>> parent of 35a8930... 更新
